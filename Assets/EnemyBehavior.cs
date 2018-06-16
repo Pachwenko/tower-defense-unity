6c445830d91caeb18bcac38d358d7387cbf1acfa
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2018-06-15 19:42:46 -0500

    fixing scripts

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Should later be changed to abstract class.
+/// A class to be inherited by every enemy. Does various functions in the game such as follows a predefined path and taking damage.
+/// </summary>
+public class EnemyBehavior : MonoBehaviour
+{
+    public float health;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = 6f;
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void addDamage(float damage)
+    {
+        if (health - damage <= 0) {
+            Destroy(gameObject);
+        }
+        health -= damage;
+    }
+}
---
+++ Assets/TowerBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to be inherited by the Towers. Used to set various propertys and perhaps do some functions.
+/// 
+/// </summary>
+public class TowerBehavior : MonoBehaviour {
+    public static float damage;
+
+    // Use this for initialization
+    void Start () {
+        damage = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.addDamage(damage);
+    }
+}
